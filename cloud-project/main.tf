732ed119690931555196694db6d3e2ce10c90ae7
gocodogo <qasimbaba22@hotmail.com>
2025-08-20 02:41:40 -0400

    Day 2: Terraform init + honeypot security group (sg) setup

---
+++ cloud-project/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Honeypot Security Group (bait SSH open)
+resource "aws_security_group" "honeypot_sg" {
+  name        = "honeypot-sg"
+  description = "Allow SSH (honeypot bait)"
+  vpc_id      = null # attach to default VPC for now
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Project = "aws-honeypot-alerting"
+  }
+}
